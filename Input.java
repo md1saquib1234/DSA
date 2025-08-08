6786b4e977e6827c174aa14b5dc43c794084a631
MD Saquib Ansari <152852816+md1saquib1234@users.noreply.github.com>
2025-08-08 21:58:42 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    //  String input = sc.next(); for word 
+    //  STRING input = sc.nextInt(); 
+    //    String name =sc.nextLine();
+    //    System.out.println(name);
+    //    System.err.println("Name");
+    //  Boolean random = sc.nextBoolean();
+     short number = Scanner.nextShort();
+        System.out.println(short);
+
+    }
+}
---
+++ loop1.java
@@ -0,0 +1,10 @@
+public class loop1 {
+    public static void main(String[] args) {
+        int counter = 0;
+        while(counter < 100) {
+            System.out.println("Hello World");
+            counter++;
+        }
+        System.out.println("Printed HW 100x");
+    }
+}
