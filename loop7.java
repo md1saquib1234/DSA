4b6e9b24b684662fae74b57252513a08d4820b6b
MD Saquib Ansari <152852816+md1saquib1234@users.noreply.github.com>
2025-08-13 17:26:33 +0530

    Add files via upload

---
+++ loop7.java
@@ -0,0 +1,14 @@
+public class loop7 {
+    public static void main(String[] args) {
+       int n = 12345678;
+
+       while( n > 0) {
+        int lastDigit = n % 10;
+        System.out.print(lastDigit + " ");
+        n = n / 10;
+
+       }
+       System.out.println();
+
+    }
+}
