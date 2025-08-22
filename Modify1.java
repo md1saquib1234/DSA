e9091a5b539c3084a2a5d974f3d6f39d71f0b499
MD Saquib Ansari <152852816+md1saquib1234@users.noreply.github.com>
2025-08-22 14:07:42 +0530

    Add files via upload

---
+++ Modify1.java
@@ -0,0 +1,10 @@
+public class Modify1 {
+    public static void main(String[] args) {
+        char ch = 'a';
+        char ch1 = 'b';
+        int number = ch;
+        int number1 = ch1;
+        System.out.println(number);
+        System.out.println(number1);
+    }
+}
