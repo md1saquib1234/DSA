d9faad6e91bbaea716188300f182e217609189b6
MD Saquib Ansari <152852816+md1saquib1234@users.noreply.github.com>
2025-08-25 10:48:33 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,11 @@
+public class Operator {
+    public static void main(String[] args) {
+       int A = 10;
+       int B = 5;
+       System.out.println("Add = "+(A+B)); 
+       System.out.println("Add = "+(A-B));
+       System.out.println("Add = "+(A*B));
+       System.out.println("Add = "+(A/B));
+       System.out.println("Add = "+(A%B));
+    }
+}
