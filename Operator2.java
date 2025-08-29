029fba7adf06de17b6e503e355b7d9d3f2c9aba3
MD Saquib Ansari <152852816+md1saquib1234@users.noreply.github.com>
2025-08-29 14:31:04 +0530

    Add files via upload

---
+++ Operator2.java
@@ -0,0 +1,13 @@
+public class Operator2 {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+    //    System.out.println(A = B);
+    //    System.out.println(A == B);
+    //    System.out.println(A > B);
+    //    System.out.println(A < B);
+    //    System.out.println(A != B);
+    //    System.out.println(A >= B);
+          System.out.println(A <= B);
+    }
+}
