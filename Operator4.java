e65bc9ceb466a97f00c45a2a14f617e99e8039ee
MD Saquib Ansari <152852816+md1saquib1234@users.noreply.github.com>
2025-08-31 20:07:23 +0530

    Add files via upload

---
+++ Operator4.java
@@ -0,0 +1,12 @@
+public class Operator4 {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+    //    System.out.println(A = B);
+    //    System.out.println(A += A);
+    //    System.out.println(B -= 5);
+    //    System.out.println(B *= B);
+        System.out.println(B /= B);
+        System.out.println(B %= B);
+    }
+}
