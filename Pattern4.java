0c6db75b9a7a533d52bfb941b12fd17ed511c989
MD Saquib Ansari <152852816+md1saquib1234@users.noreply.github.com>
2025-09-07 19:08:04 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,17 @@
+public class Pattern4 {
+    public static void main(String[] args) {
+        int n = 5;
+        char ch = 'A';
+
+        //outer loop 
+        for(int line=1; line<=n; line++) {
+            //inner loop
+            for(int chars=1; chars<=line; chars++) {
+            System.out.print(ch);
+            ch++;
+        }
+        System.out.println();
+    }
+}
+
+}
