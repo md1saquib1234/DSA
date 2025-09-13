1b7a51b69ce3a7d89c5a51541537afde900886fb
MD Saquib Ansari <152852816+md1saquib1234@users.noreply.github.com>
2025-09-13 16:25:28 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a+b;// sum of two no
+        System.out.println(sum);
+
+    }
+}
