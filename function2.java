9d7164498ce5dc7d160916c0f9a9cb379521dd27
MD Saquib Ansari <152852816+md1saquib1234@users.noreply.github.com>
2025-09-19 23:07:01 +0530

    Add files via upload

---
+++ function2.java
@@ -0,0 +1,24 @@
+
+
+public class function2 {
+
+    public static void swap(int a, int b) {
+        //swap
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+    public static void main(String[] args) {
+        int a = 5;
+        int b =10;
+
+
+     
+        swap(a,b);
+
+    }
+}
