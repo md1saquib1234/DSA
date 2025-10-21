8150a327dc4d992f98ac04113864b9dd8f36db10
MD Saquib Ansari <152852816+md1saquib1234@users.noreply.github.com>
2025-10-21 15:50:12 +0530

    Add files via upload

---
+++ clearIthbit.java
@@ -0,0 +1,9 @@
+public class clearIthbit {
+    public static int clearIBits(int n, int i) {
+        int bitMask = (~0) << i;
+        return n & bitMask;
+    }
+    public static void main(String[] args) {
+        System.out.println(clearIBits(15, 2));
+    }
+}
